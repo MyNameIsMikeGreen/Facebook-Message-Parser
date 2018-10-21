be55bfc6274547ae06ab68a35b546377a851634c
Mike Green <M.Green5@student.liverpool.ac.uk>
2018-10-21 15:15:43 +0100

    Added __main__

--- Message-Parser.py
+++ Message-Parser.py
@@ -4,8 +4,6 @@ import datetime
 import sqlite3
 import sys
 
-# Run with "python [script_filename] [Facebook_messages_archive] [alias_filename] [line_number_of_preferred_alias]"
-
 MESSAGES_FILE_NAME = sys.argv[1]    # Facebook messages file path. Archive usually "messages.htm".
 USER_ALIASES = lines = [line.rstrip('\n') for line in open(sys.argv[2])]    # Reads the alias file, puts into list.
 PREFERRED_ALIAS_INDEX = int(sys.argv[3])-1   # Line number in alias file that preferred alias appears on.
@@ -28,77 +26,81 @@ def all_elements_with_tag_and_attributes(element_list, tag_name, attribute_name,
 
 
 # Strips the timezone from a Facebook date string.
-def strip_time_zone(string):
-    if string[-3:] == "UTC": return string[:-4]
-    return string[:-7]
+def strip_time_zone(timezone_string):
+    if timezone_string[-3:] == "UTC":
+        return timezone_string[:-4]
+    return timezone_string[:-7]
 
 
 # Run a query on a SQLite database.
-def run_query(queryString):
-    cur.execute(queryString)
+def run_query(query_string, con):
+    cur = con.cursor()
+    cur.execute(query_string)
     return cur.fetchall()
 
 
-# Open the file and filter out non-printable characters
-with open (MESSAGES_FILE_NAME, "r") as file:
-    file_string=file.read()
-filtered_string = filter(lambda x: x in string.printable, file_string)  # Strip non-printable characters
+def create_database_from_html():
+    # Open the file and filter out non-printable characters
+    with open(MESSAGES_FILE_NAME, "r") as file:
+        file_string = file.read()
+    filtered_string = filter(lambda x: x in string.printable, file_string)  # Strip non-printable characters
 
-# Setup memory-based database
-con = sqlite3.connect(":memory:")
-cur = con.cursor()
-table_creation_query = "CREATE table Messages(" \
-                       "Message_ID integer primary key autoincrement, " \
-                       "Message_Text text, " \
-                       "Message_DateTime text, " \
-                       "Message_Sender text, " \
-                       "Message_Receiver text)"
-run_query(table_creation_query)
+    # Setup memory-based database
+    con = sqlite3.connect(":memory:")
+    cur = con.cursor()
+    table_creation_query = "CREATE table Messages(" \
+                           "Message_ID integer primary key autoincrement, " \
+                           "Message_Text text, " \
+                           "Message_DateTime text, " \
+                           "Message_Sender text, " \
+                           "Message_Receiver text)"
+    run_query(table_creation_query)
 
-# Form the XML structure
-root = ET.fromstring(filtered_string)
+    # Form the XML structure
+    root = ET.fromstring(filtered_string)
 
-# Narrow down to just contents
-body = first_element_with_tag(root, "body")
-contents = first_element_with_tag_and_attributes(body, "div", "class", "contents")
+    # Narrow down to just contents
+    body = first_element_with_tag(root, "body")
+    contents = first_element_with_tag_and_attributes(body, "div", "class", "contents")
 
-for block in contents:
-    if block.tag == "div":  # All threads are held in some empty container div
-        threads = all_elements_with_tag_and_attributes(block, "div", "class", "thread")  # All the threads in this div
-        for thread in threads:
+    for block in contents:
+        if block.tag == "div":  # All threads are held in some empty container div
+            threads = all_elements_with_tag_and_attributes(block, "div", "class",
+                                                           "thread")  # All the threads in this div
+            for thread in threads:
 
-            # Establish the friend name
-            thread_names = [x.strip() for x in thread.text.split(",")]
-            thread_names_without_user = [x for x in thread_names if x not in USER_ALIASES]
-            if len(thread_names_without_user) != 1: continue    # Group chats not supported
-            title = thread_names_without_user[0]
+                # Establish the friend name
+                thread_names = [x.strip() for x in thread.text.split(",")]
+                thread_names_without_user = [x for x in thread_names if x not in USER_ALIASES]
+                if len(thread_names_without_user) != 1:
+                    continue  # Group chats not supported
+                title = thread_names_without_user[0]
 
-            # Read as header/body pairs
-            for i in range(0, len(thread), 2):
-                header = first_element_with_tag_and_attributes(thread[i], "div", "class", "message_header")
+                # Read as header/body pairs
+                for i in range(0, len(thread), 2):
+                    header = first_element_with_tag_and_attributes(thread[i], "div", "class", "message_header")
 
-                message_sender = first_element_with_tag_and_attributes(header, "span", "class", "user").text
-                if message_sender in USER_ALIASES:
-                    message_sender = USER_ALIASES[PREFERRED_ALIAS_INDEX]
-                    message_receiver = title
-                else:
-                    message_receiver = USER_ALIASES[PREFERRED_ALIAS_INDEX]
+                    message_sender = first_element_with_tag_and_attributes(header, "span", "class", "user").text
+                    if message_sender in USER_ALIASES:
+                        message_sender = USER_ALIASES[PREFERRED_ALIAS_INDEX]
+                        message_receiver = title
+                    else:
+                        message_receiver = USER_ALIASES[PREFERRED_ALIAS_INDEX]
 
-                message_time_raw_string = first_element_with_tag_and_attributes(header, "span", "class", "meta").text
-                normalised_time_string = strip_time_zone(message_time_raw_string)
-                message_time = datetime.datetime.strptime(normalised_time_string, "%A, %d %B %Y at %H:%M")
+                    message_time_raw_string = first_element_with_tag_and_attributes(header, "span", "class",
+                                                                                    "meta").text
+                    normalised_time_string = strip_time_zone(message_time_raw_string)
+                    message_time = datetime.datetime.strptime(normalised_time_string, "%A, %d %B %Y at %H:%M")
 
-                message_body = thread[i + 1].text
+                    message_body = thread[i + 1].text
 
-                # No support for strings with ' currently. To be fixed in later versions.
-                if message_body is not None:
-                    message_body = message_body.replace("'", "")
-                if message_sender is not None:
-                    message_sender = message_sender.replace("'", "")
-                if message_receiver is not None:
-                    message_receiver = message_receiver.replace("'", "")
-
-                try:
+                    # No support for strings with ' currently. To be fixed in later versions.
+                    if message_body is not None:
+                        message_body = message_body.replace("'", "")
+                    if message_sender is not None:
+                        message_sender = message_sender.replace("'", "")
+                    if message_receiver is not None:
+                        message_receiver = message_receiver.replace("'", "")
                     insertion_query = "INSERT into Messages(" \
                                       "Message_Text, Message_DateTime, " \
                                       "Message_Sender, Message_Receiver) " \
@@ -106,21 +108,23 @@ for block in contents:
                                       "', '" + str(message_time) + \
                                       "', '" + str(message_sender) + \
                                       "', '" + str(message_receiver) + "')"
-                    run_query(insertion_query)
-                except sqlite3.OperationalError:
-                    print "ERROR: ", str(insertion_query)
-                    continue
-
-                print "Sender: " + str(message_sender) + ", Time: " + str(message_time) + ", Message: " + str(message_body)
-
-
-#####
-##### ALL DATA INSERTED TO DATABASE AT THIS POINT - RUN ALL CUSTOM QUERIES BELOW. #####
-#####
+                    try:
+                        run_query(insertion_query, con)
+                    except sqlite3.OperationalError:
+                        print("ERROR: ", str(insertion_query))
+                        continue
+                    print("Sender: " + str(message_sender) + ", Time: " + str(message_time) + ", Message: " + str(
+                        message_body))
+    return con
 
 
-# Example query. Output all messages to and from user account in order of sending.
-all_messages_sorted_query = run_query("SELECT Message_Sender, Message_DateTime, Message_Text FROM Messages ORDER BY datetime(Message_DateTime)")
-with open("query_output.txt", "w") as output_file:
-    for message in all_messages_sorted_query:
-        output_file.write(str(message[0])+ "\n" + str(message[1])+ "\n" + str(message[2]) + "\n\n")
+if __name__ == '__main__':
+    database_connection = create_database_from_html()
+
+    # Example query. Output all messages to and from user account in order of sending.
+    all_messages_sorted_query = run_query(
+        "SELECT Message_Sender, Message_DateTime, Message_Text FROM Messages ORDER BY datetime(Message_DateTime)",
+        database_connection)
+    with open("query_output.txt", "w") as output_file:
+        for message in all_messages_sorted_query:
+            output_file.write(str(message[0]) + "\n" + str(message[1]) + "\n" + str(message[2]) + "\n\n")
