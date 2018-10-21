0dce668e22b4ebbf8ac0b76bb1f0fd5009e873a1
Mike Green <M.Green5@student.liverpool.ac.uk>
2018-10-21 21:09:15 +0100

    Work on function for generating iNSERT SQL queries.

--- Message-Parser.py
+++ Message-Parser.py
@@ -3,57 +3,14 @@ import string
 import datetime
 import sqlite3
 import sys
-from pypika import Query, Table
+
+from htmlparser import first_element_with_tag, first_element_with_tag_and_attributes, \
+    all_elements_with_tag_and_attributes, strip_time_zone
+from sqlgenerator import SqlGenerator
 
 MESSAGES_FILE_NAME = sys.argv[1]    # Facebook messages file path. Archive usually "messages.htm".
 USER_ALIASES = lines = [line.rstrip('\n') for line in open(sys.argv[2])]    # Reads the alias file, puts into list.
 PREFERRED_ALIAS_INDEX = int(sys.argv[3])-1   # Line number in alias file that preferred alias appears on.
-
-
-def first_element_with_tag(element_list, tag_name):
-    """
-    Returns the first element in an element list with the tag name specified.
-    :param element_list: List to search.
-    :param tag_name: Tag to extract.
-    :return: First element with tag name. None if not found.
-    """
-    return next((x for x in element_list if x.tag == tag_name), None)
-
-
-def first_element_with_tag_and_attributes(element_list, tag_name, attribute_name, attribute_value):
-    """
-    Returns the first element in an element list with the tag name specified.
-    :param element_list: List to search.
-    :param tag_name: Tag to extract.
-    :param attribute_name: Attribute to find on tag.
-    :param attribute_value: Value of attribute.
-    :return: First element with tag and attributes that match. None if not found.
-    """
-    return next((x for x in element_list if (x.tag == tag_name) and
-                 (x.attrib.get(attribute_name) == attribute_value)), None)
-
-
-def all_elements_with_tag_and_attributes(element_list, tag_name, attribute_name, attribute_value):
-    """
-    Returns all elements in an element list with the tag name specified.
-    :param element_list: List to search.
-    :param tag_name: Tag to extract.
-    :param attribute_name: Attribute to find on tag.
-    :param attribute_value: Value of attribute.
-    :return: All elements that match tag and attribute values. None if not found.
-    """
-    return [x for x in element_list if (x.tag == tag_name) and (x.attrib.get(attribute_name) == attribute_value)]
-
-
-def strip_time_zone(timezone_string):
-    """
-    Strips the timezone from a Facebook date string.
-    :param timezone_string: String to strip timezone from.
-    :return: String without timezone.
-    """
-    if timezone_string[-3:] == "UTC":
-        return timezone_string[:-4]
-    return timezone_string[:-7]
 
 
 def run_query(query_string, con):
@@ -66,9 +23,6 @@ def run_query(query_string, con):
     cur = con.cursor()
     cur.execute(query_string)
     return cur.fetchall()
-
-
-
 
 
 def create_database_from_html(location=":memory:"):
@@ -84,6 +38,7 @@ def create_database_from_html(location=":memory:"):
     # Setup memory-based database
     con = sqlite3.connect(location)
 
+    # Define and create messages table
     messages_table_details = {
         "name": "Messages",
         "rows": [
@@ -110,11 +65,7 @@ def create_database_from_html(location=":memory:"):
             }
         ]
     }
-
-
-
-
-    #run_query(table_creation_query, con)
+    run_query(SqlGenerator.get_query_create_table(messages_table_details), con)
 
     # Form the XML structure
     root = ET.fromstring(filtered_string)
--- sqlgenerator.py
+++ sqlgenerator.py
@@ -7,7 +7,7 @@ class SqlGenerator(object):
         :param table_details: JSON defining tables in the form:
          {
              "name": "Messages",
-                "rows": [
+                "cols": [
                     {
                         "name": "Message_ID",
                         "type": "integer",
@@ -17,14 +17,47 @@ class SqlGenerator(object):
         }
         :return: An SQL query to create the table described.
         """
-        row_strings = []
-        for row in table_details["rows"]:
-            row_string = "{0} {1}".format(row["name"], row["type"])
-            if "attributes" in row:
-                attributes = " ".join([attribute for attribute in row["attributes"]])
-                row_string += " " + attributes
-            row_strings.append(row_string)
-        table_rows_string = ", ".join(row_strings)
+        col_strings = []
+        for col in table_details["cols"]:
+            col_string = "{0} {1}".format(col["name"], col["type"])
+            if "attributes" in col:
+                attributes = " ".join([attribute for attribute in col["attributes"]])
+                col_string += " " + attributes
+            col_strings.append(col_string)
+        table_cols_string = ", ".join(col_strings)
 
-        query = "CREATE table {0}({1})".format(table_details["name"], table_rows_string)
+        query = "CREATE table {0}({1})".format(table_details["name"], table_cols_string)
+        return query
+
+    @staticmethod
+    def get_query_insert_into_table(table_details, input_values):
+        """
+        Insert a set of values for named columns for a table that is defined with the specification.
+        :param table_details: JSON defining tables in the form:
+         {
+             "name": "Messages",
+                "cols": [
+                    {
+                        "name": "Message_ID",
+                        "type": "integer",
+                        "attributes": ["primary", "key", "autoincrement"]
+                    }
+                ]
+        }
+        :param input_values: JSON key-value pairs for column names and values to be put against those columns.
+        :return: An SQL query to insert the desired values.
+        """
+        raise NotImplementedError("This function has not yet been tested and thus should not be used yet.")
+        # Get two lists splitting the key-values in the same respective order
+        table_cols = []
+        values = []
+        for k, v in input_values:
+            table_cols.append(k)
+            values.append(v)
+
+        table_cols_str = ", ".join(table_cols)
+        # TODO: Type checking to remove "'" where necessary
+        values_str = ", ".join(["'" + str(value) + "'" for value in values])  # Add the quotes
+
+        query = "INSERT into {0}({1} VALUES {2})".format(table_details["name"], table_cols_str, values_str)
         return query
