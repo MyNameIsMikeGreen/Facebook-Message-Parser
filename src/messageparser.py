3a7bfbd0554e49e17a415a209e24e9f67d679292
Mike Green <M.Green5@student.liverpool.ac.uk>
2018-11-11 14:15:34 +0000

    Created archive object.

--- src/messageparser.py
+++ src/messageparser.py
@@ -1,5 +1,7 @@
 import argparse
 
+from zip.facebookarchive import FacebookArchive
+
 
 def parse_arguments():
     """
@@ -7,10 +9,10 @@ def parse_arguments():
     :return: System arguments.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("archive", help="Path to the Facebook archive.")
+    parser.add_argument("archive", help="Path to the Facebook archive ZIP.")
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = parse_arguments()
-
+    archive = FacebookArchive(args.archive)
